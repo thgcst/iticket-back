444f6ee41618974fcb9280f9dd974de80e39d5a0
Thiago Costa <thiagodias2708@gmail.com>
2021-10-07 19:33:45 -0300

    feat: create user test

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -32,4 +32,13 @@ describe("Create user service", () => {
       ])
     );
   });
+
+  it("should not create users with the same phone", async () => {
+    const createUser = new CreateUserService();
+    await createUser.execute({ name: "John Doe", phone: "21123121231" });
+    const response = async () =>
+      createUser.execute({ name: "John Doe2", phone: "21123121231" });
+
+    await expect(response()).rejects.toThrow();
+  });
 });
